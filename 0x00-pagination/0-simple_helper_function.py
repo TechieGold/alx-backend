aec04419b71a3a770e664d7451cc6b0321fd65ef
Gold Israel <techiegold101@gmail.com>
2024-04-01 22:25:02 +0100

    task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Contains a definition of index_page function."""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    Takes two integer arguments and returns a turple
+    of sixe containing start index and end index
+    """
+    start, end = 0, 0
+    for i in range(page):
+        start = end
+        end += page_size
+
+    return (start, end)
