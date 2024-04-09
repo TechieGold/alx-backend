1290111345d2913235e4e7ac88694e5a56aac15e
Gold Israel <techiegold101@gmail.com>
2024-04-09 21:19:42 +0100

    add condition to check if locale is in supported languages

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Basic Babel setup"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+class Config(object):
+    """ Configuration for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__, template_folder='templates')
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel()
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Select and return best language match based on supported languages.
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index() -> str:
+    """Handles index route"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
