b472c4ae544f58c48a15c2424ff7b1def6fb18e6
Gold Israel <techiegold101@gmail.com>
2024-04-09 20:37:08 +0100

    basic flask app

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Flask app """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Handles index route"""
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
